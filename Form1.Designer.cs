b0361f9b95c6ca4414d4d9568bfc882f5a5d48eb
Victor Davidsson <kontakt@victordavidsson.se>
2019-08-02 19:14:54 +0200

    Started work on color converter
    A large commit in one, sorry about that. The program can now take input as hex, #, rgb and rgba, calculate the color based on that and then spit out converted to any of the other supported colors. HSL color coming soon hopefully

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ColorConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.colorInputBox = new System.Windows.Forms.TextBox();
+            this.introLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupColorInput = new System.Windows.Forms.Panel();
+            this.subIntroLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkerboardPictureBox = new System.Windows.Forms.PictureBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hexBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rgbBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.hslBox = new System.Windows.Forms.TextBox();
+            this.groupColorInput.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.checkerboardPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colorInputBox
+            // 
+            this.colorInputBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.colorInputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorInputBox.Location = new System.Drawing.Point(3, 3);
+            this.colorInputBox.Name = "colorInputBox";
+            this.colorInputBox.Size = new System.Drawing.Size(200, 26);
+            this.colorInputBox.TabIndex = 0;
+            this.colorInputBox.WordWrap = false;
+            // 
+            // introLabel
+            // 
+            this.introLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.introLabel.AutoSize = true;
+            this.introLabel.BackColor = System.Drawing.Color.Transparent;
+            this.introLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.introLabel.Location = new System.Drawing.Point(133, 12);
+            this.introLabel.Name = "introLabel";
+            this.introLabel.Size = new System.Drawing.Size(135, 26);
+            this.introLabel.TabIndex = 1;
+            this.introLabel.Text = "Enter a color";
+            this.introLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(209, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // groupColorInput
+            // 
+            this.groupColorInput.Controls.Add(this.colorInputBox);
+            this.groupColorInput.Controls.Add(this.button1);
+            this.groupColorInput.Location = new System.Drawing.Point(70, 75);
+            this.groupColorInput.Name = "groupColorInput";
+            this.groupColorInput.Size = new System.Drawing.Size(287, 32);
+            this.groupColorInput.TabIndex = 3;
+            // 
+            // subIntroLabel
+            // 
+            this.subIntroLabel.AutoSize = true;
+            this.subIntroLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subIntroLabel.Location = new System.Drawing.Point(101, 41);
+            this.subIntroLabel.Name = "subIntroLabel";
+            this.subIntroLabel.Size = new System.Drawing.Size(203, 17);
+            this.subIntroLabel.TabIndex = 4;
+            this.subIntroLabel.Text = "Using hex, RGB, RGBA or HSL";
+            this.subIntroLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.introLabel);
+            this.panel1.Controls.Add(this.groupColorInput);
+            this.panel1.Controls.Add(this.subIntroLabel);
+            this.panel1.Location = new System.Drawing.Point(200, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 120);
+            this.panel1.TabIndex = 6;
+            // 
+            // checkerboardPictureBox
+            // 
+            this.checkerboardPictureBox.Image = global::ColorConverter.Properties.Resources.checkerboard;
+            this.checkerboardPictureBox.ImageLocation = "";
+            this.checkerboardPictureBox.InitialImage = null;
+            this.checkerboardPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.checkerboardPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.checkerboardPictureBox.Name = "checkerboardPictureBox";
+            this.checkerboardPictureBox.Size = new System.Drawing.Size(800, 200);
+            this.checkerboardPictureBox.TabIndex = 5;
+            this.checkerboardPictureBox.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.hexBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 203);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 200);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hex";
+            // 
+            // hexBox
+            // 
+            this.hexBox.BackColor = System.Drawing.SystemColors.Control;
+            this.hexBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.hexBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hexBox.Location = new System.Drawing.Point(7, 20);
+            this.hexBox.Multiline = true;
+            this.hexBox.Name = "hexBox";
+            this.hexBox.ReadOnly = true;
+            this.hexBox.Size = new System.Drawing.Size(233, 160);
+            this.hexBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rgbBox);
+            this.groupBox2.Location = new System.Drawing.Point(268, 203);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 200);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "RGB";
+            // 
+            // rgbBox
+            // 
+            this.rgbBox.BackColor = System.Drawing.SystemColors.Control;
+            this.rgbBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rgbBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rgbBox.Location = new System.Drawing.Point(7, 20);
+            this.rgbBox.Multiline = true;
+            this.rgbBox.Name = "rgbBox";
+            this.rgbBox.ReadOnly = true;
+            this.rgbBox.Size = new System.Drawing.Size(233, 160);
+            this.rgbBox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.hslBox);
+            this.groupBox3.Location = new System.Drawing.Point(524, 203);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 200);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "HSL";
+            // 
+            // hslBox
+            // 
+            this.hslBox.BackColor = System.Drawing.SystemColors.Control;
+            this.hslBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.hslBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hslBox.Location = new System.Drawing.Point(7, 20);
+            this.hslBox.Multiline = true;
+            this.hslBox.Name = "hslBox";
+            this.hslBox.ReadOnly = true;
+            this.hslBox.Size = new System.Drawing.Size(233, 160);
+            this.hslBox.TabIndex = 0;
+            this.hslBox.Text = "Coming soon!";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkerboardPictureBox);
+            this.Name = "Form1";
+            this.Text = "Colorconverter";
+            this.groupColorInput.ResumeLayout(false);
+            this.groupColorInput.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.checkerboardPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox colorInputBox;
+        private System.Windows.Forms.Label introLabel;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel groupColorInput;
+        private System.Windows.Forms.Label subIntroLabel;
+        private System.Windows.Forms.PictureBox checkerboardPictureBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox hexBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox rgbBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox hslBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace ColorConverter
+{
+    public partial class Form1 : Form
+    {
+        private Color inputColor;
+        private Regex allSupportedRegex;
+
+        public Form1()
+        {
+            InitializeComponent();
+            inputColor = Color.FromArgb(128, 255, 0, 0);
+            /* Well, this took a while to find out. It captures any supported color system in the first group, and then the parameters in seperate groups. Optionally, for rgba, it captures the fractional as a parameter */
+            allSupportedRegex = new Regex(@"^((#)|(HEX)|(RGBA)|(RGB)|(HSL))[\(\s]?(\d+|[0-9a-fA-F]{2}),?\s?(\d+|[0-9a-fA-F]{2}),?\s?(\d+|[0-9a-fA-F]{2}),?\s?(0?[\.|\,]?\d)?[\)\s]?$");
+
+            checkerboardPictureBox.Paint += new PaintEventHandler(this.checkerboardPictureBox_Paint);
+
+            updateColorTexts(inputColor);
+        }
+        
+        private void checkerboardPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush brush = new SolidBrush(inputColor);
+            Graphics graphics = e.Graphics;
+
+            graphics.FillRectangle(brush, new Rectangle(0, 0, 800, 200));
+            brush.Dispose();
+        }
+
+        private void changeColorBox(Color color)
+        {
+            inputColor = color;
+            this.Refresh();
+        }
+
+        private void updateColorTexts(Color color)
+        {
+            updateHexText(color);
+            updateRGBText(color);
+            updateHslText(color);
+        }
+
+        private void updateHexText(Color color)
+        {
+            hexBox.Text = String.Format("#RRGGBB\t#{0}{1}{2}{4}#RRGGBBAA\t#{0}{1}{2}{3}", color.R.ToString("X2"), color.G.ToString("X2"), color.B.ToString("X2"), color.A.ToString("X2"), Environment.NewLine);
+        }
+
+        private void updateRGBText(Color color)
+        {
+            rgbBox.Text = String.Format("RGB()\tRGB({0}, {1}, {2}){4}RGBA()\tRGBA({0}, {1}, {2}, {3}){4}ARGB()\tARGB({3}, {0}, {1}, {2})", color.R.ToString(), color.G.ToString(), color.B.ToString(), color.A.ToString(), Environment.NewLine);
+        }
+
+        private void updateHslText(Color color)
+        {
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+  
+            MatchCollection matches = allSupportedRegex.Matches(colorInputBox.Text);
+            if (matches.Count > 0)
+            {
+                Match match = matches[0];
+                if (match.Groups[1].Value == "#" || match.Groups[1].Value == "HEX")
+                {
+                    /* The user entered a color using either # or HEX(). ColorTranslator supports hex right out of the box so I don't need to convert it. I know that groups 7, 8, 9 are the ones that captured the parameters so let's use them */
+                    Color convertedColor = System.Drawing.ColorTranslator.FromHtml("#" + match.Groups[7] + match.Groups[8] + match.Groups[9]);
+                    changeColorBox(convertedColor);
+                    updateColorTexts(convertedColor);
+                }
+                else if (match.Groups[1].Value == "RGB")
+                {
+                    /* The user entered a color using RGB, let's create a color using Color.FromArgb but set alpha as 255 */
+                    int red = int.Parse(match.Groups[7].Value);
+                    int green = int.Parse(match.Groups[8].Value);
+                    int blue = int.Parse(match.Groups[9].Value);
+
+                    Color convertedColor = Color.FromArgb(255, red, green, blue);
+                    changeColorBox(convertedColor);
+                    updateColorTexts(convertedColor);
+                }
+                else if (match.Groups[1].Value == "RGBA")
+                {
+                    int red = int.Parse(match.Groups[7].Value);
+                    int green = int.Parse(match.Groups[8].Value);
+                    int blue = int.Parse(match.Groups[9].Value);
+                    decimal alpha = decimal.Parse(match.Groups[10].Value.Replace(".", ","));
+
+                    Color convertedColor = Color.FromArgb((int)(alpha * 255), red, green, blue);
+                    changeColorBox(convertedColor);
+                    updateColorTexts(convertedColor);
+                }
+            }
+            else
+            {
+                /* There was no match for the string provided - let the user know this. */
+            }
+        }
+    }
+}
