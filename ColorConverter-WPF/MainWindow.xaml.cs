1cf4a9db1301b72534865c344441aefdf695cbc3
Victor Davidsson <kontakt@victordavidsson.se>
2019-09-04 17:09:43 +0200

    Lots of work here, hard to summarize
    I split up the program into smaller parts, switched the Datacontext to be a model that I defined, made the data bindings work and added a lot of stuff to the view.

--- ColorConverter-WPF/MainWindow.xaml.cs
+++ ColorConverter-WPF/MainWindow.xaml.cs
@@ -23,42 +23,117 @@ namespace ColorConverter_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private InputChecker inputChecker;
-        private MyInputBrush inputBrush;
+        private readonly MyColorBrushes Model;
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = this;
-
-            inputBrush = new MyInputBrush();
-
-            Binding binding = new Binding("Color")
-            {
-                Source = inputBrush
-            };
-            InputColorBox.SetBinding(Rectangle.FillProperty, binding);
-            inputChecker = new InputChecker();
+            Model = MyColorBrushes.GetBrushes();
+            DataContext = Model;
         }
         private void InputCheckCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            inputBrush.Color = new SolidColorBrush(inputChecker.TestRegex(InputTextBox.Text));
+            Color enteredColor = InputChecker.TestRegex(InputTextBox.Text);
+
+            Model.InputColor = new SolidColorBrush(enteredColor);
+            Color websafeColor = ColorCalculator.CalculateWebsafeFromColor(enteredColor);
+            Model.WebsafeColor = new SolidColorBrush(websafeColor);
+            Model.WebsafeText = websafeColor.ToString();
+            Color complementColor = ColorCalculator.CalculateComplementFromColor(enteredColor);
+            Model.ComplementColor = new SolidColorBrush(complementColor);
+            Model.ComplementText = complementColor.ToString();
         }
     }
-    public class MyInputBrush : INotifyPropertyChanged
+    public class MyColorBrushes : INotifyPropertyChanged
     {
-        private SolidColorBrush inputColor;
-        public SolidColorBrush Color
+        SolidColorBrush _inputColor = new SolidColorBrush();
+        SolidColorBrush _websafeColor = new SolidColorBrush();
+        string _websafeText = "";
+        SolidColorBrush _complementColor = new SolidColorBrush();
+        string _complementText = "";
+        SolidColorBrush[] _harmonyColors = new SolidColorBrush[] { new SolidColorBrush(), new SolidColorBrush(), new SolidColorBrush(), new SolidColorBrush() };
+        static MyColorBrushes _brushes;
+
+        public SolidColorBrush InputColor
         {
-            get { return this.inputColor;  }
+            get { return this._inputColor; }
             set
             {
-                if(inputColor != value)
+                if (_inputColor != value)
                 {
-                    inputColor = value;
-                    NotifyPropertyChanged("Color");
+                    _inputColor = value;
+                    NotifyPropertyChanged("InputColor");
                 }
             }
         }
+        public SolidColorBrush WebsafeColor
+        {
+            get { return this._websafeColor; }
+            set
+            {
+                if (_websafeColor != value)
+                {
+                    _websafeColor = value;
+                    NotifyPropertyChanged("WebsafeColor");
+                }
+            }
+        }
+        public string WebsafeText
+        {
+            get { return this._websafeText; }
+            set
+            {
+                if (_websafeText != value)
+                {
+                    _websafeText = value;
+                    NotifyPropertyChanged("WebsafeText");
+                }
+            }
+        }
+        public SolidColorBrush ComplementColor
+        {
+            get { return this._complementColor; }
+            set
+            {
+                if (_complementColor != value)
+                {
+                    _complementColor = value;
+                    NotifyPropertyChanged("ComplementColor");
+                }
+            }
+        }
+        public string ComplementText
+        {
+            get { return this._complementText; }
+            set
+            {
+                if (_complementText != value)
+                {
+                    _complementText = value;
+                    NotifyPropertyChanged("ComplementText");
+                }
+            }
+        }
+        public SolidColorBrush[] HarmonyColors
+        {
+            get { return this._harmonyColors; }
+            set
+            {
+                if (_harmonyColors != value)
+                {
+                    _harmonyColors = value;
+                    NotifyPropertyChanged("HarmonyColors");
+                }
+            }
+        }
+        public static MyColorBrushes GetBrushes()
+        {
+            if (_brushes == null)
+            {
+                _brushes = new MyColorBrushes();
+            }
+            return _brushes;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void NotifyPropertyChanged(string propName)
