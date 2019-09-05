ac2cb4af97e4071336ca75811031a96286dd85d1
Victor Davidsson <kontakt@victordavidsson.se>
2019-09-05 16:40:25 +0200

    Added text boxes of the color in different color systems

--- ColorConverter-WPF/Converters.cs
+++ ColorConverter-WPF/Converters.cs
@@ -107,4 +107,78 @@ namespace ColorConverter_WPF
             throw new NotImplementedException();
         }
     }
+
+    public class ColorToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Color color = (Color)value;
+            string type = parameter.ToString().ToUpper();
+            if (type == "HEX")
+            {
+                string red = color.R.ToString("X2"),
+                green = color.G.ToString("X2"),
+                blue = color.B.ToString("X2"),
+                alpha = color.A.ToString("X2");
+
+                return string.Format("#RRGGBB\t#{0}{1}{2}{4}#RRGGBBAA\t#{0}{1}{2}{3}{4}#AARRGGBB\t#{3}{0}{1}{2}", red, green, blue, alpha, Environment.NewLine);
+            }
+            else if (type == "RGB")
+            {
+                byte red = color.R,
+                    green = color.G,
+                    blue = color.B,
+                    alpha = color.A;
+
+                byte redPercent = (byte)(red / 255.0M * 100.0M);
+                byte greenPercent = (byte)(green / 255.0M * 100.0M);
+                byte bluePercent = (byte)(blue / 255.0M * 100.0M);
+
+                decimal alphaDec = alpha / 255.0M;
+
+                string text = string.Format("RGB({0}, {1}, {2})", red, green, blue) +
+                string.Format("{0}", Environment.NewLine) +
+                string.Format("RGB({0}%, {1}%, {2}%)", redPercent, greenPercent, bluePercent) +
+                string.Format("{0}{0}", Environment.NewLine) +
+                string.Format("RGBA({0}, {1}, {2}, {3}){5}RGBA({0}, {1}, {2}, {4})", red, green, blue, alpha, alphaDec, Environment.NewLine) +
+                string.Format("{0}{0}", Environment.NewLine) +
+                string.Format("RGBA({0}%, {1}%, {2}%, {4}){5}RGBA({0}%, {1}%, {2}%, {3})", redPercent, greenPercent, bluePercent, alpha, alphaDec, Environment.NewLine) +
+                string.Format("{0}{0}", Environment.NewLine) +
+                string.Format("ARGB({3}, {0}, {1}, {2}){5}ARGB({4}, {0}, {1}, {2})", red, green, blue, alpha, alphaDec, Environment.NewLine) +
+                string.Format("{0}{0}", Environment.NewLine) +
+                string.Format("ARGB({3}, {0}%, {1}%, {2}%){5}ARGB({4}, {0}%, {1}%, {2})", redPercent, greenPercent, bluePercent, alpha, alphaDec, Environment.NewLine);
+                return text;
+            }
+            else if(type == "HSL")
+            {
+                ColorConverter.GetHSLFromColor(color.R, color.G, color.B, out double hue, out double saturation, out double lightness);
+
+                hue = Math.Round(hue, 0);
+                saturation = Math.Round(saturation * 100, 0);
+                lightness = Math.Round(lightness * 100, 0);
+
+                decimal alphaDec = color.A / 255.0M;
+
+                string text = string.Format("HSL({0}, {1}, {2})", hue, saturation, lightness) +
+                string.Format("{0}", Environment.NewLine) +
+                string.Format("HSL({0}, {1}%, {2}%)", hue, saturation, lightness) +
+                string.Format("{0}", Environment.NewLine) +
+                string.Format("HSL({0}°, {1}%, {2}%)", hue, saturation, lightness) +
+                string.Format("{0}{0}", Environment.NewLine) +
+                string.Format("HSLA({0}, {1}, {2}, {3})", hue, saturation, lightness, alphaDec) +
+                string.Format("{0}", Environment.NewLine) +
+                string.Format("HSLA({0}, {1}%, {2}%, {3})", hue, saturation, lightness, alphaDec) +
+                string.Format("{0}", Environment.NewLine) +
+                string.Format("HSLA({0}°, {1}%, {2}%, {3})", hue, saturation, lightness, alphaDec);
+
+                return text;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
