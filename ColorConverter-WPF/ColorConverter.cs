612770a7b32e5db214cbb6497a1e794120066328
Victor Davidsson <kontakt@victordavidsson.se>
2019-09-06 13:28:40 +0200

    Update ColorConverter.cs
    Removed unused methods, created regions and made HSL och HSV methods more robust with rounding of the result

--- ColorConverter-WPF/ColorConverter.cs
+++ ColorConverter-WPF/ColorConverter.cs
@@ -5,6 +5,7 @@ namespace ColorConverter_WPF
 {
     internal static class ColorConverter
     {
+        #region Helper methods for converters
         private static byte ConvertAlphaStringToByte(string alphaString)
         {
             byte alpha;
@@ -27,17 +28,29 @@ namespace ColorConverter_WPF
 
             return alpha;
         }
-
-        public static Color ConvertFromHexString(string hexStringR, string hexStringG, string hexStringB, string hexStringA = "FF")
+        #endregion
+        #region Hex Color method to convert
+        public static Color ConvertFromHexString(string hexStringR, string hexStringG, string hexStringB, string hexStringA)
         {
             byte red = Convert.ToByte(hexStringR, 16),
             green = Convert.ToByte(hexStringG, 16),
             blue = Convert.ToByte(hexStringB, 16),
-            alpha = Convert.ToByte(hexStringA, 16);
+            alpha;
 
-            return CreateColorFromRGB(red, green, blue, alpha);
+            /* If the alpha string isn't defined, default to 255 alpha */
+            if (hexStringA.Length == 0)
+            {
+                alpha = 255;
+            }
+            else
+            {
+                alpha = Convert.ToByte(hexStringA, 16);
+            }
+
+            return Color.FromArgb(alpha, red, green, blue);
         }
-
+        #endregion
+        #region RGB Color method to convert
         public static Color ConvertFromRGBString(string rbgStringR, string rgbStringG, string rgbStringB, string rgbStringA)
         {
             byte red, green, blue;
@@ -70,9 +83,10 @@ namespace ColorConverter_WPF
                 blue = byte.Parse(rgbStringB);
             }
 
-            return CreateColorFromRGB(red, green, blue, alpha);
+            return Color.FromArgb(alpha, red, green, blue);
         }
-
+        #endregion
+        #region HSL Color methods to convert and create
         public static Color ConvertFromHSLString(string hslStringH, string hslStringS, string hslStringL, string hslStringA)
         {
             double hue = float.Parse(hslStringH.Replace("°", "")),
@@ -83,102 +97,7 @@ namespace ColorConverter_WPF
 
             return CreateColorFromHSL(hue, saturation, lightness, alpha);
         }
-
-        public static Color ConvertFromHSVString(string hsvStringH, string hsvStringS, string hsvStringL, string hsvStringA)
-        {
-            double hue = float.Parse(hsvStringH.Replace("°", "")),
-                        saturation = float.Parse(hsvStringS.Replace("%", "")) / 100.0,
-                        value = float.Parse(hsvStringL.Replace("%", "")) / 100.0;
-
-            byte alpha = ConvertAlphaStringToByte(hsvStringA);
-
-            return CreateColorFromHSV(hue, saturation, value, alpha);
-        }
-
-        public static Color ConvertFromRGBToHSL(byte red, byte green, byte blue, byte alpha)
-        {
-            double hue, saturation, lightness;
-            double doubleR = red / 255.0,
-                doubleG = green / 255.0,
-                doubleB = blue / 255.0;
-
-            double max = doubleR;
-            if (max < doubleG)
-            {
-                max = doubleG;
-            }
-            if (max < doubleB)
-            {
-                max = doubleB;
-            }
-
-            double min = doubleR;
-            if (min > doubleG)
-            {
-                min = doubleG;
-            }
-            if (min > doubleB)
-            {
-                min = doubleB;
-            }
-
-            double diff = max - min;
-            lightness = (max + min) / 2;
-
-            if (Math.Abs(diff) < 0.00001)
-            {
-                saturation = 0;
-                hue = 0;
-            }
-            else
-            {
-                if (lightness <= 0.5)
-                {
-                    saturation = diff / (max + min);
-                }
-                else
-                {
-                    saturation = diff / (2 - max - min);
-                }
-
-                double RDist = (max - doubleR) / diff;
-                double GDist = (max - doubleG) / diff;
-                double BDist = (max - doubleB) / diff;
-
-                if (doubleR == max)
-                {
-                    hue = BDist - GDist;
-                }
-                else if (doubleG == max)
-                {
-                    hue = 2 + RDist - BDist;
-                }
-                else
-                {
-                    hue = 4 + GDist - RDist;
-                }
-
-                hue = hue * 60;
-                if (hue < 0)
-                {
-                    hue += 360;
-                }
-            }
-
-            return CreateColorFromHSL(hue, saturation, lightness, alpha);
-        }
-
-        public static Color ConvertFromRGBToHSV(byte red, byte green, byte blue, byte alpha)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Color CreateColorFromRGB(byte red, byte green, byte blue, byte alpha)
-        {
-            /* Well I was going to add some exception checks here - but I realised I already use unsigned bytes so they can't be out of range. An exception would have been thrown already if that was the case */
-            return Color.FromArgb(alpha, red, green, blue);
-        }
-
+        
         public static Color CreateColorFromHSL(double hue, double saturation, double lightness, byte alpha)
         {
             byte red, green, blue;
@@ -216,6 +135,59 @@ namespace ColorConverter_WPF
             return Color.FromArgb(alpha, red, green, blue);
         }
 
+        public static void GetHSLFromColor(byte red, byte green, byte blue, out double hue, out double saturation, out double lightness)
+        {
+            /* Code from an internet page about color theory */
+            double doubleR = red / 255.0,
+                doubleG = green / 255.0,
+                doubleB = blue / 255.0,
+                max = Math.Max(doubleR, Math.Max(doubleG, doubleB)),
+                min = Math.Min(doubleR, Math.Min(doubleG, doubleB));
+
+            double diff = max - min;
+            lightness = (max + min) / 2;
+
+            if (Math.Abs(diff) < 0.00001)
+            {
+                saturation = 0;
+                hue = 0;
+            }
+            else
+            {
+                if (lightness <= 0.5)
+                {
+                    saturation = diff / (max + min);
+                }
+                else
+                {
+                    saturation = diff / (2.0 - max - min);
+                }
+
+                double RDist = (max - doubleR) / diff;
+                double GDist = (max - doubleG) / diff;
+                double BDist = (max - doubleB) / diff;
+
+                if (doubleR == max)
+                {
+                    hue = BDist - GDist;
+                }
+                else if (doubleG == max)
+                {
+                    hue = 2.0 + RDist - BDist;
+                }
+                else
+                {
+                    hue = 4.0 + GDist - RDist;
+                }
+
+                hue *= 60.0;
+                if (hue < 0.0)
+                {
+                    hue += 360.0;
+                }
+            }
+        }
+
         private static double QqhToRGB(double q1, double q2, double hue)
         {
             if (hue > 360)
@@ -243,6 +215,18 @@ namespace ColorConverter_WPF
             {
                 return q1;
             }
+        }
+        #endregion
+        #region HSV Color methods to convert and create
+        public static Color ConvertFromHSVString(string hsvStringH, string hsvStringS, string hsvStringL, string hsvStringA)
+        {
+            double hue = float.Parse(hsvStringH.Replace("°", "")),
+                        saturation = float.Parse(hsvStringS.Replace("%", "")) / 100.0,
+                        value = float.Parse(hsvStringL.Replace("%", "")) / 100.0;
+
+            byte alpha = ConvertAlphaStringToByte(hsvStringA);
+
+            return CreateColorFromHSV(hue, saturation, value, alpha);
         }
 
         public static Color CreateColorFromHSV(double hue, double saturation, double value, byte alpha)
@@ -309,59 +293,6 @@ namespace ColorConverter_WPF
             return Color.FromArgb(alpha, red, green, blue);
         }
 
-        public static void GetHSLFromColor(byte red, byte green, byte blue, out double hue, out double saturation, out double lightness)
-        {
-            /* Code from an internet page about color theory */
-            double doubleR = red / 255.0,
-                doubleG = green / 255.0,
-                doubleB = blue / 255.0,
-                max = Math.Max(doubleR, Math.Max(doubleG, doubleB)),
-                min = Math.Min(doubleR, Math.Min(doubleG, doubleB));
-
-            double diff = max - min;
-            lightness = (max + min) / 2;
-
-            if (Math.Abs(diff) < 0.00001)
-            {
-                saturation = 0;
-                hue = 0;
-            }
-            else
-            {
-                if (lightness <= 0.5)
-                {
-                    saturation = diff / (max + min);
-                }
-                else
-                {
-                    saturation = diff / (2.0 - max - min);
-                }
-
-                double RDist = (max - doubleR) / diff;
-                double GDist = (max - doubleG) / diff;
-                double BDist = (max - doubleB) / diff;
-
-                if (doubleR == max)
-                {
-                    hue = BDist - GDist;
-                }
-                else if (doubleG == max)
-                {
-                    hue = 2.0 + RDist - BDist;
-                }
-                else
-                {
-                    hue = 4.0 + GDist - RDist;
-                }
-
-                hue *= 60.0;
-                if (hue < 0.0)
-                {
-                    hue += 360.0;
-                }
-            }
-        }
-
         public static void GetHSVFromColor(byte red, byte green, byte blue, out double hue, out double saturation, out double value)
         {
             /* Code from a StackOverflow answer */
@@ -417,5 +348,6 @@ namespace ColorConverter_WPF
                 }
             }
         }
+        #endregion
     }
 }
